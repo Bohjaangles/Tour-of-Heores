65322ee84d32a12560ae1c30b3987deee5dfe73a
bohjaangles <bohjaangles@gmail.com>
2023-03-01 16:12:55 -0600

    add a an array of heroes to populate app

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero'
+
+export const HEROES: Hero[] = [
+  {id: 1, name: 'Potatoe Man'},
+  {id: 2, name: 'Dr. CrimpsForDays'},
+  {id: 3, name: 'The guy over there'},
+  {id: 4, name: 'President Falcon'},
+  {id: 5, name: 'Froggy TruckBear'},
+  {id: 6, name: 'Professor Clippy'},
+  {id: 7, name: 'Dr. Michael Jordan'},
+  {id: 8, name: 'Pendulum Netsky'},
+  {id: 9, name: 'Whiny Child Whisperer'},
+  {id: 10, name: 'Seventeen red pandas in a trench coat named Vinny'},
+  {id: 11, name: 'Captian Quakkers'},
+];
