c6d9a06c6b646adfae18841b74a59b81f0ca1b8e
bohjaangles <bohjaangles@gmail.com>
2023-03-02 15:42:34 -0600

    add navigation and dashboard components

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,26 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from "../hero"
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+
   constructor(private heroService: HeroService) {}
-  getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
-  }
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
     this.getHeroes();
   }
-  heroes: Hero[] = [];
-  selectedHero?: Hero;
-  onSelect(hero: Hero) {
-    this.selectedHero = hero
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
+  // onSelect(hero: Hero) {
+  //   this.selectedHero = hero
+  //   this.messageService.add(`HeroesComponent: Selected Hero id=${hero.id}`)
+  // }
 }
