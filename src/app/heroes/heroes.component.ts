d431fdb3d07da281fbed454ef6c8b7eb0f12c74c
bohjaangles <bohjaangles@gmail.com>
2023-03-02 14:02:35 -0600

    add message service and component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from "../hero"
-import { HEROES } from '../mock-heroes'
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,7 +8,17 @@ import { HEROES } from '../mock-heroes'
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  constructor(private heroService: HeroService) {}
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.getHeroes();
+  }
+  heroes: Hero[] = [];
   selectedHero?: Hero;
   onSelect(hero: Hero) {
     this.selectedHero = hero
